ebb1294694efe85459de817e35a728b9e2b4f101
VuDangKhoa <dangkhoa09102004@gmail.com>
2023-12-24 23:05:28 +0700

    commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Cậu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Cảm ơn cậu rất nhiều';
+const titleCard = 'Gửi cậu lời chúc mùa Giáng Sinh';
+const contentCard = 'Chúc cậu và gia đình một mùa Giáng Sinh vui vẻ và hạnh phúc💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'IMG_3107.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
